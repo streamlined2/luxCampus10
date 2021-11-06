2b54389061ef959da144db1cae010fbb2dbd1062
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2021-11-06 20:58:09 +0200

    Fifth draft
    1. method 'copy' implemented for recursive recreation of folders and
    nested files into destination directory.

--- luxCampus10/src/main/java/org/training/campus/filemanager/FileManager.java
+++ luxCampus10/src/main/java/org/training/campus/filemanager/FileManager.java
@@ -1,15 +1,22 @@
 package org.training.campus.filemanager;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class FileManager {
+
+	private static final int BUFFER_SIZE = 10 * 1024;
 
 	public static int countFiles(String path) {
 		File root = new File(path);
 		if (!root.exists())
-			throw new IllegalArgumentException(String.format("wrong parameter: %s doesn't exist", path));
+			throw new IllegalArgumentException(String.format("folder %s doesn't exist", path));
 		if (!root.isDirectory())
-			throw new IllegalArgumentException(String.format("wrong parameter: %s should be folder, not a file", path));
+			throw new IllegalArgumentException(String.format("%s should be folder, not a file", path));
 		return countFiles(root);
 	}
 
@@ -28,9 +35,9 @@ public class FileManager {
 	public static int countDirs(String path) {
 		File root = new File(path);
 		if (!root.exists())
-			throw new IllegalArgumentException(String.format("wrong parameter: %s doesn't exist", path));
+			throw new IllegalArgumentException(String.format("folder %s doesn't exist", path));
 		if (!root.isDirectory())
-			throw new IllegalArgumentException(String.format("wrong parameter: %s should be folder, not a file", path));
+			throw new IllegalArgumentException(String.format("%s should be folder, not a file", path));
 		return countDirs(root);
 	}
 
@@ -44,9 +51,51 @@ public class FileManager {
 		return count;
 	}
 
-	public static void main(String[] args) {
-		System.out.println(countFiles("C:/1"));
-		System.out.println(countDirs("C:/1"));
+	public static void copy(String from, String to) throws IOException {
+		File source = new File(from);
+		if (!source.exists())
+			throw new IllegalArgumentException(String.format("file or folder %s doesn't exist", from));
+		File dest = new File(to);
+		if (!dest.exists())
+			throw new IllegalArgumentException(String.format("folder %s doesn't exist", to));
+		if (!dest.isDirectory())
+			throw new IllegalArgumentException(String.format("%s is not a folder", to));
+		copy(source, dest);
+	}
+
+	private static void copy(File source, File destDir) throws IOException {
+		if (source.isDirectory()) {
+			File targetDir = new File(destDir, source.getName());
+			if(targetDir.mkdir()){
+				for (File nestedFile : source.listFiles()) {
+					copy(nestedFile, targetDir);
+				}
+			}
+		} else {
+			copyFile(source, destDir);
+		}
+	}
+
+	private static void copyFile(File source, File destDir) throws IOException {
+		byte[] buffer = new byte[BUFFER_SIZE];
+		File destFile = new File(destDir, source.getName());
+		try (BufferedInputStream is = new BufferedInputStream(new FileInputStream(source))) {
+			try (BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(destFile))) {
+				int count = 0;
+				while (true) {
+					count = is.read(buffer);
+					if (count == -1)
+						break;
+					os.write(buffer, 0, count);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		//System.out.println(countFiles("C:/1"));
+		//System.out.println(countDirs("C:/1"));
+		copy("C:/1","D:/1");
 	}
 
 }
